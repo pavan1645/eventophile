05293aa3af0d6c4e87cd5a5ff14a91203fd076a7
pavan <pavan1645@gmail.com>
2019-05-27 19:57:35 +0530

    compression

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const compression = require("compression");
+
 const app = express();
 
 const PORT = 8000;
 
+app.use(compression());
 app.use(express.static(__dirname + "/public"));
 
 app.listen(process.env.PORT ||  PORT);
